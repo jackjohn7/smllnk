a6d3e94779a807bcda913b65b417d19540cbba35
= <branchjohnchristopher@gmail.com>
2024-03-02 02:37:23 -0600

    some formatting changes

--- public/views/login/login_templ.go
+++ public/views/login/login_templ.go
@@ -38,7 +38,7 @@ func LoginTemplate(baseProps layout.BaseProps, emailError string) templ.Componen
 			var templ_7745c5c3_Var2 string
 			templ_7745c5c3_Var2, templ_7745c5c3_Err = templ.JoinStringErrs(emailError)
 			if templ_7745c5c3_Err != nil {
-				return templ.Error{Err: templ_7745c5c3_Err, FileName: `public/views/login/login.templ`, Line: 25, Col: 24}
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `public/views/login/login.templ`, Line: 29, Col: 24}
 			}
 			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var2))
 			if templ_7745c5c3_Err != nil {
